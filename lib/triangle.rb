7deebcc9d6aa64c5e221b37e711a0def7c2ce6fa
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-26 14:15:09 +0000

    Done.

---
+++ lib/triangle.rb
@@ -0,0 +1,45 @@
+class Triangle
+  
+  attr_accessor :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind
+    if @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3 && !(@side_1 == 0 && @side == 0 && @side_3 == 0) && (@side_1 + @side_2 > @side_3 && @side_2 + @side_3 > @side_1 && @side_1 + @side_3 > @side_2) && !(@side_1 < 0 && @side_2 < 0 && @side_3 < 0)
+      :equilateral
+    elsif ((@side_1 == @side_2 && @side_1 != @side_3 && @side_2 != @side_3) || 
+          (@side_1 == @side_3 && @side_1 != @side_2 && @side_2 != @side_3) ||
+          (@side_3 == @side_2 && @side_1 != @side_3 && @side_1 != @side_2)) &&
+                 !(@side_1 == 0 && @side == 0 && @side_3 == 0) &&
+            (@side_1 + @side_2 > @side_3 && 
+            @side_2 + @side_3 > @side_1 && 
+            @side_1 + @side_3 > @side_2) &&
+                !(@side_1 < 0 && @side_2 < 0 && @side_3 < 0)
+      :isosceles
+    elsif (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3) &&
+           !(@side_1 == 0 && @side == 0 && @side_3 == 0) &&
+            (@side_1 + @side_2 > @side_3 && 
+            @side_2 + @side_3 > @side_1 && 
+            @side_1 + @side_3 > @side_2) &&
+                !(@side_1 < 0 && @side_2 < 0 && @side_3 < 0)
+      :scalene
+    else
+      begin 
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+      
+  end
+  
+  class TriangleError < StandardError
+   # def message
+  #    "This is custom message"
+   # end
+  end
+end
